819ff0659cf49ba9020277a1dfa4218f4dd27763
andresbm <abm56@alumnes.udl.cat>
2023-04-12 16:39:20 +0200

    need to read positions and receive REG REQ

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
+# -------------------------------------------------------------------------------
+# author:   Andres Bernardez Matalobos
+# python_version: 3.10
+# -------------------------------------------------------------------------------
 #!/usr/bin/env python3
 
 import sys
+import os
+import datetime
+import socket
+import select
+import struct
+import time
+import threading
+import random
 
+from datetime import datetime
 
 configuration_file = "server.cfg"
 acceptedclients_file = "equips.dat"
@@ -11,7 +24,7 @@ machine_data = []
 
 debug_mode = False
 
-
+IP = "127.0.0.1"
 
 def read_parameters():
     global debug_mode, server_data
@@ -60,11 +73,7 @@ def set_parameters(file):
 
     name, MAC, udp_port, tcp_port = server_data[0][1], server_data[1][1], server_data[2][1], server_data[3][1]
 
-    print("\nInformación del servidor:")
-    print("Nombre: ", name)
-    print("MAC: ", MAC)
-    print("Puerto UDP: ", udp_port)
-    print("Puerto TCP: ", tcp_port)
+
     return name, MAC, udp_port, tcp_port
 
 def initialize_machine_data(file):
@@ -73,27 +82,51 @@ def initialize_machine_data(file):
     with open(acceptedclients_file) as f:
         machine_data = f.readlines()
     machine_data = [x.strip() for x in machine_data]
-    for line in range(len(machine_data)):
-        if machine_data[line] == "":
-            machine_data.pop(line)
-    for line in range(len(machine_data)):
-        machine_data[line] = machine_data[line].split(" ")
-    for i in range (len(machine_data)):
+
+    # Transform each line of machine_data
+    for i in range(len(machine_data)):
+        if machine_data[i] == "":
+            continue
+        machine_data[i] = machine_data[i].split(" ")
         machine_data[i].insert(0, "DISCONNECTED")
-        while len(machine_data[i][1]) < 7:
-            machine_data[i][1] = "\0" + machine_data[i][1]
-        machine_data[i][2] = machine_data[i][2] + "\0"
-        machine_data[i].append("000000\0")
-        machine_data[i].append("")
-    initialize_machine_data(acceptedclients_file)
-
-
+        machine_data[i][1] = machine_data[i][1].rjust(7, "\0")
+        machine_data[i][2] += "\0"
+        machine_data[i] += ["000000\0", ""]
 
     print("Datos de las máquinas extraidos del archivo ")
     for line in machine_data:
         print(line)
 
     return machine_data
+
+
+#sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#sock_udp.bind((IP, int(udp_port)))
+
+
+def print_debug(message_debug):
+    if debug_mode:
+        print(f"{datetime.now().time().isoformat(timespec='seconds')}:{message_debug}")
+
+
+def read_commands():
+        command = input("")
+        if command.startswith("quit"):
+            print_debug("Exiting the server")
+            sock_udp.close()
+            sys.exit()
+        elif command == "list":
+            print_debug("Showing the state of the authorized clients")
+            print("STATE\t\tNAME\t\tMAC\t\tRANDOM_NUMBER\t\tIP\n")
+            for client in machine_data:
+                if client[0] == "ALIVE":
+                    print(f"{client[0]}\t\t{client[1]}\t\t{client[2]}\t{client[3]}\t\t\t{client[4]}\n")
+                else:
+                    print(f"{client[0]}\t{client[1]}\t\t{client[2]}\t{client[3]}\t\t\t{client[4]}\n")
+        else:
+            print_debug("Invalid command")
+
+
 
 
 if __name__ == '__main__':
